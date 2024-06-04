c4f57a400a46e139c6fc263f3bcbb94a4ddefa9f
Htetko510217 <htetko510217@gmai.com>
2024-06-04 22:57:29 +0700

    feat : update meals page ui layout

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,27 @@
+import { Suspense } from 'react';
+import Link from 'next/link';
+
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
 export default function MealsPage() {
   return (
-    <div>meals page</div>
-  )
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+          <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
