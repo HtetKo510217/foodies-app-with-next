37d36f1f0f3e4e3adcb231bc69b9350fc033ea0a
Htetko510217 <htetko510217@gmai.com>
2024-06-04 22:41:04 +0700

    feat : update community page ui

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,28 +1,32 @@
-import Link from "next/link";
-import logoImg from '@/assets/logo.png'
-import classes from './main-header.module.css'
-import Image from "next/image";
-import MainHeaderBackground from "./main-header-background";
+import Link from 'next/link';
+import Image from 'next/image';
+
+import MainHeaderBackground from './main-header-background';
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+import NavLink from './nav-link';
+
 export default function MainHeader() {
   return (
     <>
-    <MainHeaderBackground />
-    <header className={classes.header}>
-        <Link href="/" className={classes.logo}>
-            <Image src={logoImg} alt="Next.js Logo" priority />
-            Foodies
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
         </Link>
+
         <nav className={classes.nav}>
-            <ul>
-                <li>
-                    <Link href="/meals">Browse Meals</Link>
-                </li>
-                <li>
-                    <Link href="/community">Foodies Community</Link>
-                </li>
-            </ul>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
         </nav>
-    </header>
+      </header>
     </>
-  )
+  );
 }
