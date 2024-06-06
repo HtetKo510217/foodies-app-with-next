3345a479d3a9b62a3087b763f4280d0ae0bf3cd2
Htetko510217 <htetko510217@gmai.com>
2024-06-07 01:36:17 +0700

    feat : update saveMeal function and logic

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to create a new meal. Please try again later.</p>
+    </main>
+  );
+}
